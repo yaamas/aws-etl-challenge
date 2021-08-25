767f0877e6211a8e4c4f1fa813f4723bcd4b1b82
Yahyaa <kogam22@gmail.com>
2021-08-25 12:24:28 -0700

    Add logger

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import logging
+
 from src import data_fetch, data_transform
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 
 def main():
