b671ad69c83e3a854bc98eb233cf4c906495a141
Yahyaa <kogam22@gmail.com>
2021-08-25 00:40:43 -0700

    Finally, a working table_exists checker funtion

--- src/data_load.py
+++ src/data_load.py
@@ -8,8 +8,12 @@ from psycopg2.extensions import (
 # from sqlalchemy import create_engine
 
 
-def connect_db(user, passw):
-    return psycopg2.connect(user=user, password=passw)
+def connect_db(host, user, passw, db=None):
+    if db:
+        return psycopg2.connect(
+            host=host, dbname=db, user=user, password=passw
+        )
+    return psycopg2.connect(host=host, user=user, password=passw)
 
 
 def check_db_exists(conn, db):
@@ -18,25 +22,39 @@ def check_db_exists(conn, db):
     cur.execute(
         sql.SQL(f"SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{db}'")
     )
+
     exists = cur.fetchone()
+    cur.close()
 
-    if not exists:
-        return False
+    return exists is not None
 
 
-def check_table_exist(conn):
+def check_table_exists(conn):
     cur = conn.cursor()
+    # Take 1
+    # cur.execute(
+    #     "SELECT EXISTS(SELECT relname FROM pg_class WHERE relname='US');"
+    # )
+    #
+    # Take 2
+    # cur.execute("SELECT to_regclass('public.uks');")
+    #
+    # Take 3
     cur.execute(
-        "SELECT EXISTS(SELECT relname FROM pg_class WHERE relname='covid19_us');"
+        "SELECT EXISTS(SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'us');"
     )
-    if cur.fetchone()[0]:
-        return True
 
-    return False
+    exists = cur.fetchone()[0]
+    cur.close()
+
+    # print()
+
+    return exists
 
 
 def create_db(conn, db):
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
     cur = conn.cursor()
     cur.execute(sql.SQL(f"CREATE DATABASE {db};"))
 
@@ -52,37 +70,42 @@ def create_table(conn):
     cur = conn.cursor()
     cur.execute(
         sql.SQL(
-            "CREATE TABLE COVID19_US (id serial, date DATE PRIMARY KEY NOT NULL, cases INTEGER NOT NULL, deaths INTEGER NOT NULL, recovered INTEGER NOT NULL);"
+            "CREATE TABLE US (id SERIAL PRIMARY KEY, date DATE UNIQUE NOT NULL, cases INTEGER NOT NULL, deaths INTEGER NOT NULL, recovered INTEGER NOT NULL);"
         )
     )
     conn.commit()
     cur.close()
 
 
-def insert_data(data, data_exists):
-    pass
+def insert_data(conn, data):
+    cur = conn.cursor()
+    cur.execute(sql.SQL("SELECT COUNT(*) FROM US;"))
+    c = cur.fetchone()[0]
+    print("insert_data: ", c)
+
+    cur.close()
 
 
 def load(data):
 
-    DB = "test"  # env.
+    HOST = "localhost"  # env.
+    DB = "covid19"  # env.
     USER = "test"  # env.
     PASSWORD = "test"  # env.
 
-    # data_exists = False
-    conn = connect_db(USER, PASSWORD)
-
+    conn = connect_db(HOST, USER, PASSWORD)
     if not check_db_exists(conn, DB):
+        print("######## db exists: ", check_db_exists(conn, DB))
         create_db(conn, DB)
-        create_table(conn)
-        # data_exists = True
-    elif not check_table_exist(conn):
-        create_table(conn)
-        # data_exists = True
-
-    # insert_data(data, data_exists)
 
     conn.close()
+
+    conn = connect_db(HOST, USER, PASSWORD, DB)
+    if not check_table_exists(conn):
+        print("######## table exists: ", check_table_exists(conn))
+        create_table(conn)
+
+    insert_data(conn, data)
 
 
 if __name__ == "__main__":
@@ -94,8 +117,9 @@ if __name__ == "__main__":
     j = pd.read_csv("sample_data/jh_data.csv")
 
     m = transformation(n, j)
+    print(len(m))
 
     print(m.info(), m.head())
-    print(m.iloc[0, :])
+    # print(m.iloc[0, :])
 
     load(m)
